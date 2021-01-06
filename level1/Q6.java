be5ff120e157bca3e3ecb0d2e87a3cd3f868cdda
Cyan Lee <72456701+pandatory-hub@users.noreply.github.com>
2021-01-06 15:41:27 +0900

    나누어 떨어지는 숫자 배열

---
+++ level1/Q6.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Q6 {
+    public int[] Q6(int[] arr, int divisor) {
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i : arr) {
+            if(i%divisor == 0)
+                list.add(i);
+        }
+        if(list.isEmpty())
+            list.add(-1);
+        
+        int[] answer = new int[list.size()];
+        
+        for(int i=0; i<list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        
+        Arrays.sort(answer);
+        
+        return answer;
+    }
+}
