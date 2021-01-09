c153118d8c54eb224d1835e7e1e025caf39c9a33
Cyan Lee <72456701+pandatory-hub@users.noreply.github.com>
2021-01-09 20:28:55 +0900

    짝수와 홀수

---
+++ Q19.java
@@ -0,0 +1,15 @@
+class Q19 {
+    public String Q19(int num) {
+        
+//         String answer = "";
+        
+//         if(num%2==0)
+//             answer = "Even";
+//         else
+//             answer = "Odd";
+        
+//         return answer;
+        
+        return num % 2 == 0 ? "Even" : "Odd";
+    }
+}
