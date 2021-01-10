fc784febc75e889e9697830445335a14ba405702
Cyan Lee <72456701+pandatory-hub@users.noreply.github.com>
2021-01-10 21:58:43 +0900

    자연수 뒤집어 배열로 만들기

---
+++ level1/Q23.java
@@ -0,0 +1,13 @@
+class Q23 {
+    public int[] Q23(long n) {
+        String s = String.valueOf(n);
+        StringBuilder sb = new StringBuilder(s);
+        sb = sb.reverse();
+        String[] ss = sb.toString().split("");
+        
+        int[] answer = new int[ss.length];
+        for(int i=0; i<ss.length; i++)
+            answer[i] = Integer.parseInt(ss[i]);
+        return answer;
+    }
+}
