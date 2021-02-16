4b4610d1b39601780f0d5b0a2dbb7391d26f08d2
Cyan Lee <72456701+pandatory-hub@users.noreply.github.com>
2021-02-16 17:11:04 +0900

    소수 판

---
+++ basic/B7.java
@@ -0,0 +1,18 @@
+public class B7 {
+    public static void main(String[] args) {
+        int num = 13;
+        boolean isPrimeNum = true;
+
+        for(int i=2; i < num/2; i++) {
+            if(num % i == 0) {
+                isPrimeNum = false;
+            }
+        }
+        if(isPrimeNum) {
+            System.out.println(num + "은 소수");
+        }
+        else {
+            System.out.println(num + "은 소수 아님");
+        }
+    }
+}
