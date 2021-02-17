c8a723adb86087af5be05e8c84a52a6a62301b6d
Cyan Lee <72456701+pandatory-hub@users.noreply.github.com>
2021-02-17 11:02:59 +0900

    숫자 사각형4

---
+++ basic/B13.java
@@ -0,0 +1,20 @@
+public class B13 {
+    public static void main(String[] args) {
+        int n = 4;
+        int[][] arr = new int[n][n];
+
+
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                arr[i][j] = (i + 1) * (j + 1);
+            }
+        }
+
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                System.out.printf("%4d", arr[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
